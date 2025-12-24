e321404b3afab308bd9473967b7bdad30dbcb0c7
Shenmarukai <shanemulc@comcast.net>
2025-12-23 21:20:54 -0500

    Fix default audio source device.

--- configuration.nix
+++ configuration.nix
@@ -41,25 +41,44 @@
 		alsa.support32Bit = true;
 		pulse.enable = true;
 
-		wireplumber.extraConfig = {
-			"force-line1-default-source" = {
-				"monitor.alsa.rules" = [
-					{
-						matches = [
-							{
-								"node.name" = "alsa_input.usb-Generic_USB_Audio-00.HiFi__Line1__source";
-							}
-						];
-						actions = {
-							update-props = {
-								"priority.session" = 2050;
-								"node.nick" = "USB Line-In";
-							};
-						};
-					}
-				];
-			};
-		};
+    wireplumber.extraConfig = {
+      "force-x2u-default-source" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              {
+                "node.name" = "alsa_input.usb-Shure_Incorporated_Shure_Digital-00.analog-stereo";
+              }
+            ];
+            actions = {
+              update-props = {
+                "priority.session" = 2200;
+                "node.nick" = "X2u Mic";
+              };
+            };
+          }
+        ];
+      };
+
+      "force-usb-speakers-default-sink" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              {
+                "node.name" = "alsa_output.usb-Generic_USB_Audio-00.HiFi__Speaker__sink";
+              }
+            ];
+            actions = {
+              update-props = {
+                "priority.session" = 2200;
+                "node.nick" = "USB Speakers";
+              };
+            };
+          }
+        ];
+      };
+    };
+
 	};
 
 	hardware.graphics = {
