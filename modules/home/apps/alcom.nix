b3cd8cf51ee66587f102c2d3cf1fbfba9347f3d8
Shenmarukai <shanemulc@comcast.net>
2026-05-26 22:41:35 -0400

    Update

--- modules/home/apps/alcom.nix
+++ modules/home/apps/alcom.nix
@@ -1,31 +1,29 @@
 { pkgs, ... }:
+
 let
-  # The fixed launcher script we made earlier
-  alcom-fix = pkgs.writeShellScriptBin "alcom-fix" ''
+  alcom-fixed = pkgs.writeShellScriptBin "ALCOM" ''
     unset WAYLAND_DISPLAY
     export GDK_BACKEND=x11
     export WEBKIT_DISABLE_DMABUF_RENDERER=1
-    exec ${pkgs.alcom}/bin/alcom "$@"
+    export WEBKIT_DISABLE_COMPOSITING_MODE=1
+    exec ${pkgs.alcom}/bin/ALCOM "$@"
   '';
 in {
   home.packages = [
-    (pkgs.alcom.overrideAttrs (old: {
-      postInstall = (old.postInstall or "") + "rm -rf $out/share/applications";
-    }))
-    alcom-fix
+    alcom-fixed
+
     (pkgs.makeDesktopItem {
       name = "alcom";
       desktopName = "ALCOM";
-      exec = "${alcom-fix}/bin/alcom-fix";
+      exec = "${alcom-fixed}/bin/ALCOM";
       icon = "alcom";
+      categories = [ "Utility" ];
     })
   ];
 
-  # This is the "Nix way": Generating the settings file with dynamic paths
-  home.file.".local/share/VRChatCreatorCompanion/settings.json".text = builtins.toJSON {
-    pathToUnityHub = "${pkgs.unityhub}/bin/unityhub";
-    # Point this to where Unity Hub actually installs editors on your disk
-    unityEditorsFolder = "/home/shane/Unity/Hub/Editor";
-    # Add other VCC settings here if needed
-  };
+  home.file.".local/share/VRChatCreatorCompanion/settings.json".text =
+    builtins.toJSON {
+      pathToUnityHub = "${pkgs.unityhub}/bin/unityhub";
+      unityEditorsFolder = "/home/shane/Unity/Hub/Editor";
+    };
 }
